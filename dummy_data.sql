90f10b3462b699a9117c794909a475228492c9c5
Andrew Walker <awalker@shareableink.com>
2016-05-11 17:56:21 -0500

    added user check

---
+++ dummy_data.sql
@@ -0,0 +1,391 @@
+--normal month
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-01 09:00:00 -5:00', '2016-01-01 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-04 09:00:00 -5:00', '2016-01-04 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-05 09:00:00 -5:00', '2016-01-05 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-06 09:00:00 -5:00', '2016-01-06 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-07 09:00:00 -5:00', '2016-01-07 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-08 09:00:00 -5:00', '2016-01-08 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-11 09:00:00 -5:00', '2016-01-11 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-12 09:00:00 -5:00', '2016-01-12 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-13 09:00:00 -5:00', '2016-01-13 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-14 09:00:00 -5:00', '2016-01-14 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-15 09:00:00 -5:00', '2016-01-15 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-18 09:00:00 -5:00', '2016-01-18 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-19 09:00:00 -5:00', '2016-01-19 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-20 09:00:00 -5:00', '2016-01-20 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-21 09:00:00 -5:00', '2016-01-21 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-22 09:00:00 -5:00', '2016-01-22 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-25 09:00:00 -5:00', '2016-01-25 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-26 09:00:00 -5:00', '2016-01-26 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-27 09:00:00 -5:00', '2016-01-27 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-28 09:00:00 -5:00', '2016-01-28 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-01-29 09:00:00 -5:00', '2016-01-29 17:00:00 -5:00');
+
+--more than 24 hour shift and less than 8 hours between
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-01 09:00:00 -5:00', '2016-02-01 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-04 09:00:00 -5:00', '2016-02-04 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-05 09:00:00 -5:00', '2016-02-05 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-06 09:00:00 -5:00', '2016-02-06 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-07 09:00:00 -5:00', '2016-02-07 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-08 09:00:00 -5:00', '2016-02-09 09:05:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-11 09:00:00 -5:00', '2016-02-11 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-12 09:00:00 -5:00', '2016-02-12 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-13 09:00:00 -5:00', '2016-02-13 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-14 09:00:00 -5:00', '2016-02-14 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-15 09:00:00 -5:00', '2016-02-15 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-18 09:00:00 -5:00', '2016-02-18 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-19 09:00:00 -5:00', '2016-02-19 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-20 09:00:00 -5:00', '2016-02-20 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-21 09:00:00 -5:00', '2016-02-21 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-22 09:00:00 -5:00', '2016-02-23 13:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-25 09:00:00 -5:00', '2016-02-25 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-26 09:00:00 -5:00', '2016-02-26 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-27 09:00:00 -5:00', '2016-02-27 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-28 09:00:00 -5:00', '2016-02-28 20:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-02-29 01:00:00 -5:00', '2016-02-29 10:00:00 -5:00');
+
+--over 80 hour work week
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-01 09:00:00 -5:00', '2016-03-01 09:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-04 09:00:00 -5:00', '2016-03-04 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-05 09:00:00 -5:00', '2016-03-05 23:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-06 09:00:00 -5:00', '2016-03-06 23:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-07 09:00:00 -5:00', '2016-03-07 23:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-08 09:00:00 -5:00', '2016-03-08 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-09 09:00:00 -5:00', '2016-03-10 08:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-08 09:00:00 -5:00', '2016-03-08 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-11 09:00:00 -5:00', '2016-03-11 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-12 09:00:00 -5:00', '2016-03-12 23:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-13 09:00:00 -5:00', '2016-03-13 23:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-14 09:00:00 -5:00', '2016-03-14 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-15 09:00:00 -5:00', '2016-03-15 23:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-18 09:00:00 -5:00', '2016-03-18 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-19 09:00:00 -5:00', '2016-03-19 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-20 09:00:00 -5:00', '2016-03-20 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-21 09:00:00 -5:00', '2016-03-21 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-22 09:00:00 -5:00', '2016-03-22 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-24 04:00:00 -5:00', '2016-03-24 22:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-25 03:00:00 -5:00', '2016-03-25 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-26 03:00:00 -5:00', '2016-03-26 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-27 03:00:00 -5:00', '2016-03-27 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-28 09:00:00 -5:00', '2016-03-28 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-03-29 09:00:00 -5:00', '2016-03-29 17:00:00 -5:00');
+
+--no full day off per week
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-01 09:00:00 -5:00', '2016-04-01 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-02 09:00:00 -5:00', '2016-04-02 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-03 09:00:00 -5:00', '2016-04-03 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-04 09:00:00 -5:00', '2016-04-04 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-05 09:00:00 -5:00', '2016-04-05 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-06 09:00:00 -5:00', '2016-04-06 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-07 09:00:00 -5:00', '2016-04-07 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-08 09:00:00 -5:00', '2016-04-08 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-09 09:00:00 -5:00', '2016-04-09 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-10 09:00:00 -5:00', '2016-04-10 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-11 09:00:00 -5:00', '2016-04-11 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-12 09:00:00 -5:00', '2016-04-12 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-13 09:00:00 -5:00', '2016-04-13 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-14 09:00:00 -5:00', '2016-04-14 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-15 09:00:00 -5:00', '2016-04-15 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-16 09:00:00 -5:00', '2016-04-16 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-17 09:00:00 -5:00', '2016-04-17 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-18 09:00:00 -5:00', '2016-04-18 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-19 09:00:00 -5:00', '2016-04-19 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-20 09:00:00 -5:00', '2016-04-20 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-21 09:00:00 -5:00', '2016-04-21 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-22 09:00:00 -5:00', '2016-04-22 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-23 09:00:00 -5:00', '2016-04-23 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-24 09:00:00 -5:00', '2016-04-24 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-25 09:00:00 -5:00', '2016-04-25 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-26 09:00:00 -5:00', '2016-04-26 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-27 09:00:00 -5:00', '2016-04-27 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-28 09:00:00 -5:00', '2016-04-28 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-29 09:00:00 -5:00', '2016-04-29 17:00:00 -5:00');
+insert into shifts 
+	(user_id, start_dt, end_dt) 
+values 
+	(1, '2016-04-30 09:00:00 -5:00', '2016-04-30 17:00:00 -5:00');
--- index.js
+++ index.js
@@ -4,24 +4,14 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-//app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-
 app.get('/', function(request, response) {
 	response.send('Woooo!');
 });
 
-app.listen(app.get('port'), function() {
-	console.log('Node app is running on port', app.get('port'));
-});
-
-app.get('/users', function (request, response) {
+app.get('/api/users', function (request, response) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 		if (err)
-				{ console.error(err); response.send("Error " + err); }
+			{ console.error(err); response.send("Error " + err); }
 		client.query('SELECT * FROM users', function(err, result) {
 			done();
 			if (err)
@@ -30,4 +20,55 @@ app.get('/users', function (request, response) {
 				{ response.send({results: result.rows}); }
 		});
 	});
-})
+});
+
+app.put('/api/:user/shift', function(request, response) {
+	findUser(
+		request.params.user,
+		function() {response.send("Couldn't find user!");}, 
+		function() {response.send("Found user!")})
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
+});
+
+
+
+
+
+function findUser(user, not_found_func, found_func) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		if (err) { 
+			console.error(err); 
+			not_found_func();
+			return;
+		}
+		client.query('SELECT * FROM users', function(err, result) {
+			done();
+			if (err) { 
+				console.error(err); 
+				not_found_func();
+				return;
+			}
+			else {
+				var found = false;
+				for(var row in result.rows) {
+					if(row['email'] === user) {
+						found = true;
+						break;
+					}
+				}
+
+				if(found) {
+					console.log('found them!');
+					found_func();
+				}
+				else {
+					console.log('not found!');
+					not_found_func();
+				}
+			}
+		});
+	});
+}
