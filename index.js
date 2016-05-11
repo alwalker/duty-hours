3718f026040e90b45953bd9be16996393ab2cc80
Andrew Walker <awalker@shareableink.com>
2016-05-10 22:09:48 -0500

    test heroku with db

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var pg = require('pg');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -10,11 +11,23 @@ app.set('port', (process.env.PORT || 5000));
 // app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send('Woooo');
+	response.send('Woooo!');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
 
-
+app.get('/users', function (request, response) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		if (err)
+				{ console.error(err); response.send("Error " + err); }
+		client.query('SELECT * FROM users', function(err, result) {
+			done();
+			if (err)
+				{ console.error(err); response.send("Error " + err); }
+			else
+				{ response.send({results: result.rows}); }
+		});
+	});
+})
