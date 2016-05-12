b37dd35c7ab3a4b4e6dea99746ad60f83bdf72fd
Andrew Walker <awalker@shareableink.com>
2016-05-11 19:43:22 -0500

    added shift get for user

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.put('/api/:user/shift', function(request, response) {
 		function(user) {
 			pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 				if (err) { 
-					console.error(err); response.send("Error " + err); 
+					console.error(err);
+					response.send("Error adding shift to database"); 
 				}
 				client.query(
 					'INSERT INTO shifts (user_id, start_dt, end_dt) VALUES($1,$2,$3)',
@@ -30,6 +31,34 @@ app.put('/api/:user/shift', function(request, response) {
 						}
 						else { 
 							response.send('Ok!'); 
+						}
+					});
+			});
+		});
+});
+
+//we are going to assume date only here for simplifcation
+app.get('/api/:user/shifts/start/:start/end/:end', function(request, response) {
+	findUser(
+		request.params.user,
+		function() {response.send("Couldn't find user!");}, 
+		function(user) {
+			pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+				if (err) { 
+					console.error(err); 
+					response.send("Error looking up shifts"); 
+				}
+				client.query(
+					"SELECT start_dt, end_dt FROM shifts WHERE user_id = $1 AND start_dt >= $2 AND end_dt <= $3",
+					[user['id'], request.params.start, request.params.end],
+					function(err, result) {
+						done();
+						if (err) { 
+							console.error(err); 
+							response.send("Error looking up shifts"); 
+						}
+						else { 
+							response.send(result.rows); 
 						}
 					});
 			});
